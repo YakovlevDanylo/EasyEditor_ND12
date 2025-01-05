207a80dd1c98e774779f02db46503693930277f1
YakovlevDanylo <166537891+YakovlevDanylo@users.noreply.github.com>
2025-01-05 12:03:09 +0200

    Create image_processor.py

---
+++ image_processor.py
@@ -0,0 +1,34 @@
+import os.path
+from PIL import Image
+from PIL.ImageQt import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class ImageProcessor:
+    def __init__(self):
+        self.image = None
+        self.dir = None
+        self.filename = None
+        self.save_dir = "Modified/"
+
+
+    def loadImage(self, dir, filename):
+        self.dir = dir
+        self.filename = filename
+        image_path = os.path.join(self.dir, self.filename)
+        self.image = Image.open(image_path)
+
+    def showImage(self, path, lb_image):
+        lb_image.hide()
+        pixmapimage = QPixmap(path)
+        w, h = lb_image.width(), lb_image.height()
+        pixmapimage = pixmapimage.scale(w, h, Qt.KeepAspectRatio)
+        lb_image.setPixmap(pixmapimage)
+        lb_image.show()
+
+    def saveImage(self):
+        path = os.path.join(self.dir, self.save_dir)
+        if not (os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
