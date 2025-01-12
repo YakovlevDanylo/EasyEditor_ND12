5229c453e446f4c80b6a3b9720f17889882acd93
YakovlevDanylo <166537891+YakovlevDanylo@users.noreply.github.com>
2025-01-12 12:10:48 +0200

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -1,7 +1,13 @@
-import os.path
+import os
 from PIL import Image
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
+from PIL.ImageFilter import (
+   BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+   EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN,
+   GaussianBlur, UnsharpMask
+)
+
 
 class ImageProcessor:
     def __init__(self):
@@ -9,15 +15,16 @@ class ImageProcessor:
         self.dir = None
         self.filename = None
         self.save_dir = "Modified/"
-
+        self.lb_image = None
 
     def loadImage(self, dir, filename):
         self.dir = dir
         self.filename = filename
-        image_path = os.path.join(self.dir, self.filename)
+        image_path = os.path.join(dir, filename)
         self.image = Image.open(image_path)
 
     def showImage(self, path, lb_image):
+        self.lb_image = lb_image
         lb_image.hide()
         pixmapimage = QPixmap(path)
         w, h = lb_image.width(), lb_image.height()
@@ -32,3 +39,34 @@ class ImageProcessor:
         image_path = os.path.join(path, self.filename)
         self.image.save(image_path)
 
+    def do_bw(self):
+        self.image = self.image.convert("L")
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path, self.lb_image)
+
+    def do_left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path, self.lb_image)
+
+    def do_right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path, self.lb_image)
+
+    def do_flip(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path, self.lb_image)
+
+    def do_red(self):
+        self.image = self.image.convert("L")
+        red_tint = Image.new("RGB", self.image.size, (255, 0, 0))
+        self.image = Image.blend(self.image.convert("RGB"), red_tint, 0.3)
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path, self.lb_image)
